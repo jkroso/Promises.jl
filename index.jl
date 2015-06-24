2ad42736ee9b3309126ac0ea144101437540c601
Jake Rosoman <jkroso@gmail.com>
2015-06-24 20:39:32 +1200

    do some renaming

--- index.jl
+++ index.jl
@@ -1,24 +1,47 @@
-abstract Promise{T}
+##
+# Deferred's are placeholders for a value which isn't
+# yet known. Though we may know what type it will be
+# hence Deferred takes the type of its eventual value
+# as a parameter
+#
+# Julia's Tasks provide everything Deferreds provide though
+# they also do a lot more which makes me feel uncomfortable
+# about building abstractions on top of them which treat
+# them as simple values
+#
+abstract Deferred{T}
 
-type Deferred{T} <: Promise{T}
-  fn::Function
+##
+# When you want to get the actual value of a deferred you
+# just call `need` on it. It's safe to call `need` on all
+# types so if in doubt...need it
+#
+function need(x::Any) x end
+
+##
+# Computation's are intended to enable lazy evaluation
+#
+type Computation{T} <: Deferred{T}
+  thunk::Function
   state::Symbol
   value::T
   error::Exception
-  Deferred(f::Function) = new(f, :pending)
+  Computation(f::Function) = new(f, :pending)
 end
 
-Deferred(f::Function) = Deferred{Any}(f)
+# Make the type parameter optional
+Computation(f::Function) = Computation{Any}(f)
 
 ##
-# Evaluate the value
+# The first time a Computation is needed it's thunk is called and the
+# result is stored. Whether it return or throws. From then on it
+# will just replicates this result without re-running the thunk
 #
-function need(x::Any) x end
-function need(d::Deferred)
+function need(d::Computation)
   d.state == :evaled && return d.value
   d.state == :failed && rethrow(d.error)
   try
-    d.value = d.fn()
+    d.value = d.thunk()
     d.state = :evaled
     d.value
   catch e
@@ -29,14 +52,14 @@ function need(d::Deferred)
 end
 
 test("need") do
-  array = Deferred(vcat)
+  array = Computation(vcat)
   @test need(1) == 1
   @test need(array) == {}
   @test need(array) === need(array)
-  @test isa(@catch(need(Deferred(error))), ErrorException)
+  @test isa(@catch(need(Computation(error))), ErrorException)
   test("with types") do
-    @test need(Deferred{Vector}(vcat)) == {}
-    @test isa(@catch(need(Deferred{Vector}(string))), MethodError)
+    @test need(Computation{Vector}(vcat)) == {}
+    @test isa(@catch(need(Computation{Vector}(string))), MethodError)
   end
 end
 
@@ -46,47 +69,47 @@ end
 #
 macro defer(body)
   if isa(body, Expr) && body.head == symbol("::")
-    :(Deferred{$(esc(body.args[2]))}(() -> $(esc(body.args[1]))))
+    :(Computation{$(esc(body.args[2]))}(()-> $(esc(body.args[1]))))
   else
-    :(Deferred{Any}(() -> $(esc(body))))
+    :(Computation{Any}(()-> $(esc(body))))
   end
 end
 
 test("@defer") do
   @test need(@defer 1) == 1
   @test isa(@catch(need(@defer error())), ErrorException)
-  @test typeof(@defer 1) == Deferred{Any}
-  @test typeof(@defer 1::Int) == Deferred{Int}
+  @test typeof(@defer 1) == Computation{Any}
+  @test typeof(@defer 1::Int) == Computation{Int}
 end
 
 ##
-# Async Promises are capable of pausing the current_task
-# thread while waiting for another thread to complete if
-# it needs to
+# Values are intended to provide a way for asynchronous processes
+# to communicate their result to other threads
 #
-type Async{T} <: Promise{T}
+type Value{T} <: Deferred{T}
   cond::Condition
   state::Symbol
   value::T
   error::Exception
-  Async() = new(Condition(), :pending)
-  Async(c::Condition) = new(c, :pending)
+  Value() = new(Condition(), :pending)
+  Value(c::Condition) = new(c, :pending)
 end
 
-Async() = Async{Any}()
+# Make the type optional
+Value() = Value{Any}()
 
-function need(promise::Async)
-  promise.state == :evaled && return promise.value
-  promise.state == :failed && rethrow(promise.error)
-  promise.state == :needed && wait(promise.cond)
+function need(v::Value)
+  v.state == :evaled && return v.value
+  v.state == :failed && rethrow(v.error)
+  v.state == :needed && return wait(v.cond)
   try
-    promise.state = :needed
-    promise.value = wait(promise.cond)
-    promise.state = :evaled
-    promise.value
+    v.state = :needed
+    v.value = wait(v.cond)
+    v.state = :evaled
+    v.value
   catch e
-    promise.state = :failed
-    promise.error = e
+    v.state = :failed
+    v.error = e
     rethrow(e)
   end
 end
@@ -94,39 +117,39 @@ end
 ##
 # Give the Promise its value
 #
-function Base.write(promise::Async, value)
-  if promise.state == :pending
-    promise.state = :evaled
-    promise.value = value
+function Base.write(v::Value, value)
+  if v.state == :pending
+    v.state = :evaled
+    v.value = value
   end
-  notify(promise.cond, value)
+  notify(v.cond, value)
 end
 
 test("write") do
-  promise = Async()
-  task = @async need(promise)
-  write(promise, 1)
-  @test need(promise) == 1
+  result = Value()
+  task = @async need(result)
+  write(result, 1)
+  @test need(result) == 1
   @test wait(task) == 1
 end
 
 ##
 # Put the Promise into a failed state
 #
-function Base.error(promise::Async, e::Exception)
-  if promise.state == :pending
-    promise.state = :failed
-    promise.error = e
+function Base.error(v::Value, e::Exception)
+  if v.state == :pending
+    v.state = :failed
+    v.error = e
   end
-  notify(promise.cond, e; error=true)
+  notify(v.cond, e; error=true)
 end
 
 test("error") do
-  promise = Async()
-  task = @async need(promise)
-  error(promise, ErrorException(""))
-  @test isa(@catch(need(promise)), ErrorException)
-  @test @catch(need(promise)) === @catch wait(task)
-  @test task.result == promise.error
+  result = Value()
+  task = @async need(result)
+  error(result, ErrorException(""))
+  @test isa(@catch(need(result)), ErrorException)
+  @test @catch(need(result)) === @catch wait(task)
+  @test task.result == result.error
   @test task.state == :failed
 end
